81645918beca72bccec3fe36e010b4ace0c2d2a5
Patrick Yoba <yobapatrick2@gmail.com>
2025-12-28 01:39:17 +0100

    modified:   model/load_model.py

--- model/load_model.py
+++ model/load_model.py
@@ -1,7 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import LoraConfig, get_peft_model
 
 
-dossier= "E:\chargement_donnees"
+dossier= r"E:\chargement_donnees"
 
 # On ajoute cache_dir pour le tokenizer
 tokenizer = AutoTokenizer.from_pretrained(
@@ -15,3 +16,18 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map="auto",
     cache_dir=dossier
 )
+
+
+
+lora_config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    lora_dropout=0.05,
+    target_modules=["q_proj", "v_proj"],
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+
+
+model = get_peft_model(model, lora_config)
+model.print_trainable_parameters()
